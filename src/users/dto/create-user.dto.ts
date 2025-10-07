e92d71d3ef2892550ecd92a6d857f5d9fb2204fb
= <=>
2025-10-06 19:04:46 -0600

    Agregando funcionalidad CRUD

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,24 @@
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateUserDto {
+    @ApiProperty ({required: true, example: 'usuario@empresa.com'})
+
+    email: string;
+
+    @ApiProperty ({required: true, example: 'John Doe'})
+    name: string;
+
+    @ApiProperty ({required: true, example: '8966-2494'})
+    telephone: string;
+
+    username?: string;
+
+    @ApiProperty ({required: true, example: 'pasword123'})
+    password: string;
+
+    @ApiProperty ({required: true, example: 1, description: 'Id del Tenant'})
+    tenantId: number;
+
+
+
+}
