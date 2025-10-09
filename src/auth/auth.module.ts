fa052ecf4ac5b0979ea5590dfd2bea246b39546b
= <=>
2025-10-08 23:06:24 -0600

    Agregando archivos strategy y guards

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,15 +2,20 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
+    PassportModule,
+    PrismaModule,
     JwtModule.register({
       secret: 'Colddriven02',
       signOptions: { expiresIn: '30m'}
     }),
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
